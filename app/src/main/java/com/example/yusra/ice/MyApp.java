ceefc6f90428fd8059775e15ddaf73f9bd50b0e1
yusra <nicehope21@gmail.com>
2015-08-25 23:14:20 +0400

    Initial commit

---
+++ app/src/main/java/com/example/yusra/ice/MyApp.java
@@ -0,0 +1,20 @@
+package com.example.yusra.ice;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MyApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_app);
+
+    }
+
+
+}
